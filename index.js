2d0d3fe14cdf707b5388fdaab375a3d10da9cfc0
Matt Weber <mathteacherone@gmail.com>
2025-01-15 14:22:50 -0500

    Vault Secured

---
+++ index.js
@@ -0,0 +1,8 @@
+let code1;
+code1 = 7 + 3;
+const code2 = (89 - 9) / 2;
+const code3 = 80 % 41;
+const message = "The vault has been secured. The combination is: ";
+const codeA = code1 + "-" + code2 + "-" + code3;
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(message, codeA, codeB);
